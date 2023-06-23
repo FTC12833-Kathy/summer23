f77fac8952272bc8e692267a2e1797958f88c58b
Kathy <jackathy@htva.net>
2023-06-23 11:21:34 -0400

    Begin S2_Lift control

--- src/main/java/org/firstinspires/ftc/teamcode/summer2023b/S2_Lift.java
+++ src/main/java/org/firstinspires/ftc/teamcode/summer2023b/S2_Lift.java
@@ -1,14 +1,17 @@
 package org.firstinspires.ftc.teamcode.summer2023b;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.Servo;
 
 public class S2_Lift {
     private LinearOpMode opMode;
+    private DcMotor liftMotor = null;
+    private Servo pivot = null;
+    private DigitalChannel bottomLimit;
 
-
-    private DigitalChannel limitSwitch;
-
+    final double LIFT_SPEED = 0.3;
 
     public S2_Lift(LinearOpMode opMode) {
         this.opMode = opMode;
@@ -16,11 +19,20 @@ public class S2_Lift {
     }
 
     public void runLift() {
+//        while (opMode && )
 //        code to run lift mechanism
+        if (opMode.gamepad2.right_trigger > 0.1){
+            liftMotor.setPower(LIFT_SPEED);
+        } else if (opMode.gamepad2.left_trigger > 0.1) {
+            liftMotor.setPower(-LIFT_SPEED);
+        } else {
+            liftMotor.setPower(0);
+        }
     }
 
     private void init() {
-//        lift = opMode.hardwareMap.get(DcMotor.class, "Lift");
+        liftMotor = opMode.hardwareMap.get(DcMotor.class, "Lift");
+        pivot = opMode.hardwareMap.get(Servo.class, "Pivot");
+        bottomLimit = opMode.hardwareMap.get(DigitalChannel.class, "BottomLimit");
     }
-
 }
