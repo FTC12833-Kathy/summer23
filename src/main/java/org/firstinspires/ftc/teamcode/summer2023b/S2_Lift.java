5cab3a16c1883529c25b69e50047e7b5dddd0a4f
ftc12833-p2 <ftc12833p2@gmail.com>
2023-07-25 17:45:19 -0400

    Add auto button heights

--- src/main/java/org/firstinspires/ftc/teamcode/summer2023b/S2_Lift.java
+++ src/main/java/org/firstinspires/ftc/teamcode/summer2023b/S2_Lift.java
@@ -11,16 +11,18 @@ public class S2_Lift {
     private Servo pivot = null;
     private DigitalChannel bottomLimit;
 
-    final double LIFT_SPEED = 1;
-    // final double TICKS_PER_REVOLUTION = 383.6; for 13:7 GoBilda motor
+    final double MANUAL_LIFT_SPEED = 1;
+    final double AUTO_LIFT_SPEED = .6;
+
+    //  383.6 ticks per rev for 13:7 GoBilda motor
     final double MAX_HEIGHT = 2000;
     final int LIFT_INCREMENT = 12;
     final int PIVOT_CLEARANCE = 650;
     int targetTicks = 0;
-    int numXPressed = 0;
 
     boolean pivotForward = true;
-    boolean isHandled = false;
+    boolean pivotIsHandled = false;
+    boolean limitIsHandled = false;
 
     public S2_Lift(LinearOpMode opMode) {
         this.opMode = opMode;
@@ -36,45 +38,51 @@ public class S2_Lift {
         opMode.telemetry.addData("pivot", pivot.getPosition());
 
         if (opMode.gamepad2.right_trigger > 0.1){ //up
-            if (isTriggered(bottomLimit)) {
+            if (isTriggered(bottomLimit)) {// sets motor mode
                 liftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             }
-            if (targetTicks + LIFT_INCREMENT < MAX_HEIGHT) {
+            if (targetTicks + LIFT_INCREMENT < MAX_HEIGHT) {// updates the slide ticks
                 targetTicks += LIFT_INCREMENT;
                 liftMotor.setTargetPosition(targetTicks);
-                liftMotor.setPower(LIFT_SPEED);
+                liftMotor.setPower(MANUAL_LIFT_SPEED);
             }
         } else if (opMode.gamepad2.left_trigger > 0.1) { //down
-            if (isTriggered(bottomLimit)) {
+
+            if (isTriggered(bottomLimit)) {//stops slide for going too far down
                 liftMotor.setPower(0);
                 targetTicks = 0;
                 liftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
                 liftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
             } else {
-                targetTicks -= LIFT_INCREMENT;
+                targetTicks -= LIFT_INCREMENT; // updates slide ticks
                 liftMotor.setTargetPosition(targetTicks);
-                liftMotor.setPower(LIFT_SPEED);
+                liftMotor.setPower(MANUAL_LIFT_SPEED);
             }
         }
 
-        if (opMode.gamepad2.x && !isHandled){
-            if (pivotForward && liftMotor.getCurrentPosition() >= PIVOT_CLEARANCE){
+        if (opMode.gamepad2.x && !pivotIsHandled){ // pivot
+            if (pivotForward && liftMotor.getCurrentPosition() >= PIVOT_CLEARANCE){//pivot an flip too low
                 pivot.setPosition(0);
                 pivotForward = false;
             } else if (liftMotor.getCurrentPosition() >= PIVOT_CLEARANCE){
                 pivot.setPosition(1);
                 pivotForward = true;
             }
-            isHandled = true;
-        } else if (!opMode.gamepad2.x && isHandled){
-            isHandled = false;
+            pivotIsHandled = true;
+        } else if (!opMode.gamepad2.x && pivotIsHandled){
+            pivotIsHandled = false;
         }
 
-        if(opMode.gamepad2.y){
-            liftMotor.setTargetPosition(1942);
-            targetTicks = 1942;
-            pivot.setPosition(0);
-            pivotForward = false;
+        if(opMode.gamepad2.y){// auto medium height
+            liftMotor.setTargetPosition(1994);
+            targetTicks = 1994;
+            liftMotor.setPower(AUTO_LIFT_SPEED);
+        }
+
+        if(opMode.gamepad2.b){//auto collect height
+            liftMotor.setTargetPosition(0);
+            targetTicks = 0;
+            liftMotor.setPower(AUTO_LIFT_SPEED);
         }
 
 
@@ -84,7 +92,7 @@ public class S2_Lift {
 
     }
 
-    public boolean isTriggered(DigitalChannel limit) {
+    public boolean isTriggered(DigitalChannel limit) {// fixes the limit get state
         return !limit.getState();
     }
 
